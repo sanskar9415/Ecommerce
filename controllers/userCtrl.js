473c285691d5e67e73dae36127a58f1bf5b60d7a
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-08-06 00:06:39 +0530

    Error Handling

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,21 +1,15 @@
 const User = require("../models/userModel");
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req,res) => {
-    try {
+const createUser = asyncHandler( async (req,res) => {
         const email = req.body.email;
         const findUser = await User.findOne({ email : email });
         if(!findUser){
             const newUser = await User.create(req.body);
             res.json(newUser);
         }else {
-            res.json({
-                msg: "User already exist",
-                success: false
-            });
+           throw new Error("User Already Exists");
         }
-    } catch (error) {
-        console.log("error in userctrl");
-    }
-}
+});
 
 module.exports = { createUser };
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dbConnect = require("./config/dbConnect");
 const dotenv = require("dotenv").config();
 const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/api/user", authRouter);
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running  at PORT ${PORT}`);
