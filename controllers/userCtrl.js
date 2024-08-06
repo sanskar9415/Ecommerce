72ef5c237136c695952c45616b602faeba2f5107
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-08-06 21:27:49 +0530

    JWT token generation

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwttoken");
 const User = require("../models/userModel");
 const asyncHandler = require('express-async-handler');
 
@@ -16,7 +17,14 @@ const loginUserCtrl = asyncHandler(async(req,res) => {
     const { email, password } = req.body;
     const findUser = await User.findOne({ email });
     if(findUser && await findUser.isPasswordMatched(password)){
-        res.json(findUser);
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            firstname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
     }else {
         throw new Error("Invalid Credentials")
     }
